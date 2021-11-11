4800b472b3885228bf214f47646001bd0479bf86
SjoerdFijnje <91532925+SjoerdFijnje@users.noreply.github.com>
2021-11-11 15:51:27 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
 # jupyter nbconvert --to script Streamlit.ipynb
 # pip install streamlit
 
@@ -8,10 +5,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-'Dit is een stukje tekst :)'
-
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
-
-st.map(map_data)
+x = st.slider('x')  # 👈 this is a widget
+st.write(x, 'squared is', x * x)
