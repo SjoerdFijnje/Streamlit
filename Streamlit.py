8b8ad3f61db9838d7ecb271e952256b2a88ce6ed
SjoerdFijnje <91532925+SjoerdFijnje@users.noreply.github.com>
2021-11-11 15:34:59 +0100

    Add files via upload

--- Streamlit.py
+++ Streamlit.py
@@ -13,22 +13,19 @@
 # pip install streamlit
 
 
-# In[33]:
+# In[2]:
 
 
 import streamlit as st
-
-
-# In[34]:
-
-
 import pandas as pd
+import numpy as np
 
-st.write("Here's our first attempt at using data to create a table:")
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+
+# In[3]:
+
+
+dataframe = np.random.randn(10, 20)
+st.dataframe(dataframe)
 
 
 # In[ ]:
