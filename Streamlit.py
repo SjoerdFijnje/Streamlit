86da220da157bb8c712c6f56b9956f865c765c62
SjoerdFijnje <91532925+SjoerdFijnje@users.noreply.github.com>
2021-11-11 15:41:46 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,38 +1,13 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[31]:
-
-
 # jupyter nbconvert --to script Streamlit.ipynb
-
-
-# In[32]:
-
-
 # pip install streamlit
-
-
-# In[2]:
-
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 
-
-# In[3]:
-
-
-chart_data = pd.DataFrame(
-     np.random.randn(20, 3),
-     columns=['a', 'b', 'c'])
-
-st.line_chart(chart_data)
-
-
-# In[ ]:
-
-
-
-
+map_data = pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
