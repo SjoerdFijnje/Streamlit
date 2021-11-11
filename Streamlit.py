9d6e9b6dba66319e558fde84ae234438d6ab8853
SjoerdFijnje <91532925+SjoerdFijnje@users.noreply.github.com>
2021-11-11 15:54:36 +0100

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -5,5 +5,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-x = st.slider('x')  # 👈 this is a widget
-st.write(x, 'squared is', x * x)
+st.text_input("Your name", key="name")
+
+# You can access the value at any point with:
+st.session_state.name
